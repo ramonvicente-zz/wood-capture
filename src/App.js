07305aa42e6897468418a87d62fef837b3763e4f
Ramon Vicente <ramon.vicente@thoughtworks.com>
2021-05-09 17:25:08 -0300

    feat: connect git status

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import './App.css';
 import firebase from "./firebase/firebase"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
 export default class App extends Component {
   constructor(){
     super();
@@ -18,8 +26,11 @@ export default class App extends Component {
       this.setState({
       image: e.target.files[0]
     })
+    this.setState({
+      downloadURL: URL.createObjectURL(e.target.files[0])
+    })
   }
-    // console.log(e.target.files[0])
+    console.log(this.state.progress)
 }
 
 handleUpload = () =>{
@@ -27,7 +38,8 @@ handleUpload = () =>{
   let file = this.state.image;
   var storage = firebase.storage();
   var storageRef = storage.ref();
-  var uploadTask = storageRef.child('folder/' + file.name).put(file);
+  var uploadTask = storageRef.child('folder/' + file.name + Date.now()).put(file);
+  var database = firebase.firestore();
 
   uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
     (snapshot) =>{
@@ -38,11 +50,21 @@ handleUpload = () =>{
     },() =>{
       // uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) =>{
 
-      uploadTask.snapshot.ref.getDownloadURL().then((url) =>{
-        this.setState({
-          downloadURL: url
-        })
+    uploadTask.snapshot.ref.getDownloadURL().then((url) =>{
+      this.setState({
+        downloadURL: url
       })
+      database.collection("woods").add({
+        name: document.getElementById("woodName").value,
+        image: url
+      })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+    })
     document.getElementById("file").value = null
 
    }
@@ -52,23 +74,48 @@ handleUpload = () =>{
 
   render() {
     return (
-      <div className="App">
-        <h4>upload image</h4>
-        <label>
-          Choose file
-        <input type="file" id="file" onChange={this.handleChange} />        
-        </label>
-
-        {this.state.progress}
-        <button className="button" onClick={this.handleUpload}>Upload</button>
-        <img
-          className="ref"
-          src={this.state.downloadURL || "https://via.placeholder.com/400x300"}
-          alt="Uploaded Images"
-          height="300"
-          width="400"
-        />
-      </div>
+      <Container>
+        <Row className="justify-content-center text-center">
+          <Jumbotron>
+            <h1 className="">Olá, seja bem vindo(a)!</h1>
+            <p>
+              Insira a imagem de uma madeira e informe seu respectivo nome para que possamos armazenar no nosso banco de dados.
+            </p>
+        </Jumbotron>
+        </Row>
+        <Row className="justify-content-md-center text-center">
+            <Col md="8" className="welcome">
+                <img
+                  className="ref"
+                  src={this.state.downloadURL || "https://via.placeholder.com/400x300"}
+                  alt="Uploaded Images"
+                  height="300"
+                  width="400"
+                />
+            </Col>
+        </Row>
+        <Row className="justify-content-md-center  text-center mt-2">
+          <Col md="8" className="welcome">
+            <input type="text" placeholder="Nome da Madeira" id="woodName"></input>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center text-center mt-2">
+            <Col md="8" className="welcome">
+            <label>
+              Escolher imagem
+              <input type="file" id="file" onChange={this.handleChange} />        
+            </label> {' '}
+            <Button onClick={this.handleUpload}>Cadastrar</Button>
+            </Col>
+        </Row>
+        {this.state.progress > 0 && this.state.progress <=100 ?
+        <Row className="justify-content-md-center text-center">
+             <Col md="8" className="welcome">
+              <ProgressBar animated now={this.state.progress} label={`${this.state.progress}%`}/>
+            </Col>
+        </Row>
+        : ''}
+      </Container>
     )
   }
 }
